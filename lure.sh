265b0aaa358905195592a602370d33bb18bb113d
BlazingRush <ember.profet@outlook.com>
2025-01-29 11:47:04 +0100

    test

--- lure.sh
+++ lure.sh
@@ -6,7 +6,9 @@ homepage='https://www.ffmpeg.org/'
 maintainer="Ember Profet <ember.profet@outlook.com>"
 architectures=('amd64' 'arm64')
 license=('custom')
-provides=('ffmpeg' 'libavcodec61' 'libavdevice61' 'libavfilter10' 'libavformat61' 'libavutil59' 'libpostproc58' 'libswresample5' 'libswscale8')
+provides=('ffmpeg')
+
+deps_ubuntu=('libavcodec-dev' 'libavdevice-dev' 'libavfilter-dev' 'libavformat-dev' 'libavutil-dev' 'libpostproc-dev' 'libswresample-dev' 'libswscale-dev')
 
 sources_arm64=("https://github.com/EternallyEmber/FFmpeg-Builds/releases/download/latest/ffmpeg-n${version}-latest-linuxarm64-nonfree-shared-${version}.tar.xz")
 checksums_arm64=("SKIP")
@@ -16,7 +18,5 @@ checksums_amd64=("SKIP")
 
 package() {
 	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-shared-${version}/bin/*; do install -Dm755 -t ${pkgdir}/usr/bin/ $f; done
-	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-shared-${version}/lib/*.so*; do install -Dm644 -t ${pkgdir}/usr/lib/ $f; done
-	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-shared-${version}/include/*; do install -Dm444 -t ${pkgdir}/usr/include/ $f; done
 	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-shared-${version}/LICENSE.txt; do install -Dm644 -t ${pkgdir}/usr/share/licenses/ffmpeg/LICENSE.txt $f; done
 }
