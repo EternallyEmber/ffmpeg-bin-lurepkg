aa677a091adaa5b928d053427ce340ee01ef494b
Yannick Profet <Yannick.Profet@leerling.berkenschutse.nl>
2025-02-03 11:39:24 +0100

    sigh

--- lure.sh
+++ lure.sh
@@ -15,6 +15,7 @@ sources_amd64=("https://github.com/EternallyEmber/FFmpeg-Builds/releases/downloa
 checksums_amd64=("SKIP")
 
 package() {
-	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-${version}/bin/*; do install -Dm755 $f -t ${pkgdir}/usr/bin/; done
-	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-${version}/man; do install -Dm755 $f -t ${pkgdir}/usr/local/share/man; done
+	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-${version}/bin/ffmpeg; do install -Dm755 /usr/bin/ffmpeg $f; done
+	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-${version}/bin/ffplay; do install -Dm755 /usr/bin/ffplay $f; done
+	for f in ${srcdir}/ffmpeg-n${version}-latest-*-nonfree-${version}/bin/ffprobe; do install -Dm755 /usr/bin/ffprobe $f; done
 }
